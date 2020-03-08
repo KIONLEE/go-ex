13fbf8b79357dcd2d41ca1e1f1fae417c3a33e99
keonlee <rjsdlwwkd@kaist.ac.kr>
2020-03-08 20:33:36 +0900

    1.3

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/KIONLEE/learngo/ex"
+	"strings"
 )
 
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
 func main() {
-	fmt.Println("hello world!")
-	ex.ExSay()
+	// fmt.Println("hello world!")
+	// ex.ExSay()
+	// fmt.Println(multiply(2, 3))
+	// totalLength, upperName := lenAndUpper("keon")
+	// fmt.Println(totalLength, upperName)
+	repeatMe("I", "am", "a", "rockstar", "!!!")
 }
