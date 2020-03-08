9c864e7a2b169c95cc8e071b57965264e3ea93b5
keonlee <rjsdlwwkd@kaist.ac.kr>
2020-03-08 18:58:15 +0900

    ~1.2

---
+++ ex/ex.go
@@ -0,0 +1,8 @@
+package ex
+
+import "fmt"
+
+func ExSay() {
+	name := "EX"
+	fmt.Println("this from outside package called", name)
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/KIONLEE/learngo/ex"
+)
+
+func main() {
+	fmt.Println("hello world!")
+	ex.ExSay()
+}
